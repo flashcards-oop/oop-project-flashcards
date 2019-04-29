93ebe73ee451c021747c6f043a42fe55b414a2d3
devmentality <nikita.volnov.1999@mail.ru>
2019-04-29 11:55:00 +0500

    Client added

---
+++ FlashcardsClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using RestSharp;
+
+namespace FlashcardsClient
+{
+    class Program
+    {
+        static void AddUser(string userName)
+        {
+            var client = new RestClient("http://localhost:17720");
+            var request = new RestRequest("api/users/create");
+            request.AddJsonBody(userName);
+
+            var response = client.Post(request);
+            Console.WriteLine(response.StatusCode);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            AddUser("Vika");
+        }
+    }
+}
