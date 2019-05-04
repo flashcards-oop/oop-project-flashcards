c0d73467392015b0a5f350190b53837bc4fe47ed
devmentality <nikita.volnov.1999@mail.ru>
2019-05-04 17:31:51 +0500

    More tests

--- FlashcardsApiTests/CardsControllerTests.cs
+++ FlashcardsApiTests/CardsControllerTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using FlashcardsApi.Controllers;
+using FlashcardsApi.Models;
 using FakeItEasy;
 using Flashcards;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,28 @@ namespace FlashcardsApiTests
 
             result.Result.Should().BeOfType<OkObjectResult>();
             (result.Result as OkObjectResult).Value.Should().BeEquivalentTo(cards);
+        }
+
+        [Test]
+        public void CreateCard_ShouldAddNewCard()
+        {
+            var cardDto = new CardDto()
+            {
+                Term = "term",
+                Definition = "definition"
+            };
+            var fakeStorage = A.Fake<IStorage>();
+            var controller = new CardsController(fakeStorage, ControllerTestsHelper.GetStandardAuthorizationService());
+            ControllerTestsHelper.AttachUserToControllerContext(controller, "admin");
+
+            var result = controller.CreateCard(cardDto);
+            result.Should().BeOfType<CreatedAtRouteResult>();
+
+            A.CallTo(() => fakeStorage.AddCard(A<Card>.That.Matches(
+                card => card.Term == cardDto.Term &&
+                        card.Definition == cardDto.Definition &&
+                        card.OwnerLogin == "admin")))
+                .MustHaveHappened();
         }
     }
 }
--- FlashcardsApiTests/ControllerTestsHelper.cs
+++ FlashcardsApiTests/ControllerTestsHelper.cs
@@ -6,17 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
+using FlashcardsApi;
+using Microsoft.Extensions.Options;
 
 namespace FlashcardsApiTests
 {
     public class ControllerTestsHelper
     {
-        public static IAuthorizationService GetStandardAuthorizationService()
+        public static IAuthorizationService GetStandardAuthorizationService(bool successful = true)
         {
             var fakeAuth = A.Fake<IAuthorizationService>();
             A.CallTo(() => fakeAuth.AuthorizeAsync(new ClaimsPrincipal(), new object(), "Any"))
                 .WithAnyArguments()
-                .Returns(Task.FromResult(AuthorizationResult.Success()));
+                .Returns(Task.FromResult(successful ? AuthorizationResult.Success() : AuthorizationResult.Failed()));
             return fakeAuth;
         }
 
